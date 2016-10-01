1d51543b497ff24169b922bdd96ba4ba8e33461f
DeBraid <de.braid@gmail.com>
2016-10-01 17:43:32 -0400

    style and copy

--- src/App.js
+++ src/App.js
@@ -8,25 +8,28 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={headshot} className="App-logo" alt="headshot" />
-          <h2>Hello World</h2>
-          <h2>I'm Derek Braid a software developer from Toronto</h2>
+          <h3>Hello World, I'm Derek Braid</h3>
+          <h3>I develop software with javascript, study biochemistry and behavioural finance.</h3>
         </div>
-        <p>Visit <a href="http://cacheflow.ca">cacheflow.ca</a> for my mission and CV.</p>
-        <p>I write about:&nbsp;
-          <a href="https://github.com/DeBraid/investing-notes">
-            investing, taxes, and policy
-          </a>,&nbsp;
-          <a href="https://github.com/DeBraid/bitcoin-notes" target="_blank">
-            bitcoin and ethereum
-          </a>,&nbsp;
-          <a href="https://github.com/DeBraid/tech-notes" target="_blank">
-            software and compsci
-          </a>, and&nbsp;
-          <a href="https://twitter.com/Puck_Quant" target="_blank">
-            hockey
-          </a>.
-        </p>
-        <p>Contact me on Twitter <a href="https://twitter.com/@Royal_Arse">@Royal_Arse</a></p>
+        <div className="App-body">
+          <p>Visit <a href="http://cacheflow.ca">cacheflow.ca</a> for my mission and CV.</p>
+          <p>I write about:&nbsp;
+            <a href="https://github.com/DeBraid/investing-notes">
+              investing, taxes, and policy
+            </a>,&nbsp;
+            <a href="https://github.com/DeBraid/bitcoin-notes" target="_blank">
+              bitcoin and ethereum
+            </a>,&nbsp;
+            <a href="https://github.com/DeBraid/tech-notes" target="_blank">
+              software and compsci
+            </a>, and&nbsp;
+            <a href="https://twitter.com/Puck_Quant" target="_blank">
+              hockey
+            </a>.
+          </p>
+          <p>Doing research on <a href="https://github.com/DeBraid/ham-innov-essay">technology as catayst</a> for regional economic devlopment.  Book coming soon...</p>
+          <p>Contact me on Twitter <a href="https://twitter.com/@Royal_Arse">@Royal_Arse</a></p>
+        </div>
       </div>
     );
   }
