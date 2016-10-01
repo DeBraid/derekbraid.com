4595f61cf430f93a542213c088f1ed48e9736bf6
DeBraid <de.braid@gmail.com>
2016-10-01 16:07:27 -0400

    moar copy improvements for homepage

--- src/App.js
+++ src/App.js
@@ -10,7 +10,21 @@ class App extends Component {
           <img src={headshot} className="App-logo" alt="headshot" />
           <h2>Hello World, I'm Derek Braid.</h2>
         </div>
-        <p>My current homepage is found at <a href="http://cacheflow.ca">cacheflow.ca</a></p>
+        <p>I'm a software developer from Toronto, Canada. My mission and CV is at <a href="http://cacheflow.ca">cacheflow.ca</a></p>
+        <p>I write about:&nbsp;
+          <a href="https://github.com/DeBraid/investing-notes">
+            investing, taxes, and policy
+          </a>,&nbsp;
+          <a href="https://github.com/DeBraid/bitcoin-notes" target="_blank">
+            bitcoin and ethereum
+          </a>,&nbsp;
+          <a href="https://github.com/DeBraid/tech-notes" target="_blank">
+            software and compsci
+          </a>, and&nbsp;
+          <a href="https://twitter.com/Puck_Quant" target="_blank">
+            hockey
+          </a>.
+        </p>
         <p>Contact me on Twitter <a href="https://twitter.com/@Royal_Arse">@Royal_Arse</a></p>
       </div>
     );
