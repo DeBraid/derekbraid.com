75cb73243bd40b8ab5d4e5dd4afa1ef7df678f5d
DeBraid <de.braid@gmail.com>
2017-03-03 13:40:52 -0500

    stub out route for podcast

--- src/AppRouter.js
+++ src/AppRouter.js
@@ -2,10 +2,12 @@ import React from 'react';
 import { Router, Route, hashHistory } from 'react-router'
 import App from './App';
 import LogPageView from './LogPageView';
+import PodcastHome from './PodcastHome';
 
 const routes = (
 	<Router history={hashHistory} onUpdate={LogPageView}>
 		<Route path="/" component={App}/>
+		<Route path="/podcast" component={PodcastHome}/>
 	</Router>
 );
 
---
+++ src/PodcastHome.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import TrackedLink from './TrackedLink';
+// import './PodcastHome.css';
+
+class PodcastHome extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Podcast Home</h1>
+      </div>
+    );
+  }
+}
+
+export default PodcastHome;
