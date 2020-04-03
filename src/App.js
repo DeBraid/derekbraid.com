4018942a39e38c5ff0beb4204c036079f0aafde4
DeBraid <de.braid@gmail.com>
2020-04-03 11:06:29 -0400

    remove reference to cacheflow

--- src/App.js
+++ src/App.js
@@ -13,32 +13,25 @@ class App extends Component {
           <h3>I develop software with javascript, study biochemistry and behavioural finance.</h3>
         </div>
         <div className="App-body">
-          <p>Visit&nbsp; 
-            <TrackedLink 
-              href="http://cacheflow.ca"
-              text="cacheflow.ca"
-            />
-            &nbsp;for my mission and CV.
-          </p>
           <p>I write about:&nbsp;
-          <TrackedLink 
+          <TrackedLink
               href="https://github.com/DeBraid/medicine"
               text="medicine and plant-based nutrition"
             />
             ,&nbsp;
-            <TrackedLink 
+            <TrackedLink
               href="https://github.com/DeBraid/investing-notes"
               text="investing, taxes, and policy"
             />
             ,&nbsp;
-            <TrackedLink 
-              href="https://github.com/DeBraid/bitcoin-notes" 
+            <TrackedLink
+              href="https://github.com/DeBraid/bitcoin-notes"
               text="bitcoin and ethereum"
             />
             ,&nbsp;
-            <TrackedLink 
+            <TrackedLink
               href="https://github.com/DeBraid/tech-notes"
-              text="software & computer science"  
+              text="software & computer science"
             />
             , and&nbsp;
             <TrackedLink
@@ -46,15 +39,15 @@ class App extends Component {
               text="hockey"
             />.
           </p>
-          <p>Doing research on&nbsp; 
-            <TrackedLink 
+          <p>Doing research on&nbsp;
+            <TrackedLink
               href="https://github.com/DeBraid/ham-innov-essay"
               text="technology as catalyst"
             /> for regional economic development.  Book coming soon...</p>
-          <p>Contact me via Twitter&nbsp; 
-            <TrackedLink 
+          <p>Contact me via Twitter&nbsp;
+            <TrackedLink
               href="https://twitter.com/@Royal_Arse"
-              text="@Royal_Arse" 
+              text="@Royal_Arse"
             />
           </p>
         </div>
